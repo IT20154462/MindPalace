6689a5ef7b7308adf1b871ce29dbd8b1ae5f867a
W.K.Sachintha Gayashan <87464058+IT20154462@users.noreply.github.com>
2023-10-06 06:52:01 +0530

    Sample commit

---
+++ app/src/main/java/com/example/myquiceaplication/QuestionAnswer.java
@@ -0,0 +1,30 @@
+package com.example.myquiceaplication;
+
+public class QuestionAnswer {
+
+    public static String qustion[] = {
+            "Which company owns the android?",
+            "Which one is not the programing language?",
+            "Where you are watching this video?",
+            "Which company owns the Apple?"
+    };
+    public static String choices[][] = {
+            {"Google", "Apple", "Nokia", "Samsung"},
+            {"Java", "Kotlin", "Notepad", "Python"},
+            {"Facebook", "Whatsapp", "Instagram", "Youtube"},
+            {"Google", "Apple", "Nokia", "Samsung"}
+
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Youtube",
+            "Apple"
+    };
+
+
+
+
+
+}
